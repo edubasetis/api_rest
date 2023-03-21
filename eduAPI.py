7b25cfd42821e528e914629795072adab445f8d7
edubasetis <eduardo.pena@basetis.com>
2023-03-21 10:27:17 +0100

    eduAPI & test working

--- eduAPI.py
+++ eduAPI.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, Path, Query # No me deja importar HTTPExceptions 
+from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel
 from typing import Annotated, Optional
-#from starlette.exceptions import HTTPExceptions
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
@@ -16,33 +16,31 @@ class UpdateItem(BaseModel):
     price: Optional[float] = None
 
 
-inventory = {}
+inventory = {
+    0 : {"name" : "name", "description": "description", "price" : 1000},
+}
 
-# Como crear un inventario y actualizarlo??
-# Se pueden definir varias funciones en cada endpoint??
-# Repasar los status codes
-# async necesario?
-# Annotated vs Optional vs | None = None
-
-# Run the API: uvicorn eduAPI:app --reload
+@app.get("/")
+async def root():
+    return {"message" : "Hello World"}
 
 @app.get("/item/{item_id}")
-async def get_item(item_id : int = Path(..., description = "The ID of the item you'd like to view.")):
+async def get_item(item_id : int = Path(..., description = "The ID of the item you'd like to view.", ge=0)):
     if item_id not in inventory:
-        raise HTTPExceptions(status_code = 404, detail = "Item ID not found.")
+        raise HTTPException(status_code = 404, detail = "Item ID not found.")
     return inventory[item_id]
 
-@app.get("/item/get-by-name") # Probando pasar el nombre como paramtro Query
-async def get_item(name : str = Query(..., title = "Name", description = "Name of the item.")):
+"""@app.get("/item/get-by-name") # Probando pasar el nombre como paramtro Query
+async def get_item_by_name(name : str = Query(..., title = "Name", description = "Name of the item.")):
     for item in inventory:
-        if inventory[item].name == name:
-            return inventory[item]
-    raise HTTPExceptions(status_code = 404, detail = "Item name not found.")
+        if inventory[item]["name"] == name:
+            return jsonable_encoder(inventory[item])
+    raise HTTPException(status_code = 404, detail = "Item name not found.")"""
 
 @app.post("/create-item/{item_id}")
-async def create_item(item_id : int, item : Item):
+async def create_item(item : Item, item_id : int = Path(..., description = "The ID of the item you'd like to create", ge=0)) :
     if item_id in inventory:
-        raise HTTPExceptions(status_code = 400, detail = "Item ID already exists.")
+        raise HTTPException(status_code = 400, detail = "Item ID already exists.")
     
     inventory[item_id] = item
     return inventory[item_id]
@@ -50,23 +48,23 @@ async def create_item(item_id : int, item : Item):
 @app.put("/update-item/{item_id}")
 async def update_item(item_id : int, item : UpdateItem):
     if item_id not in inventory:
-        raise HTTPExceptions(status_code = 404, detail = "Item ID does not exists.")
+        raise HTTPException(status_code = 404, detail = "Item ID does not exists.")
     
     if item.name != None:
-        inventory[item_id].name = item.name
+        inventory[item_id]["name"] = item.name
 
     if item.description != None:
-        inventory[item_id].description = item.description
+        inventory[item_id]["description"] = item.description
     
     if item.price != None:
-        inventory[item_id].price = item.price
+        inventory[item_id]["price"] = item.price
 
     return inventory[item_id]
 
 @app.delete("/delete-item") # Probando pasar item_id como parametro Query
 async def delete_item(item_id : int = Query(..., description = "The ID of th item to delete.")):
     if item_id not in inventory:
-        raise HTTPExceptions(status_code = 404, detail = "Item ID does not exists.")
+        raise HTTPException(status_code = 404, detail = "Item ID does not exists.")
     
     del inventory[item_id]
     return {"Success" : "Item deleted"}
--- test_eduAPI.py
+++ test_eduAPI.py
@@ -1,12 +1,70 @@
 from fastapi.testclient import TestClient
 
-from .eduAPI import app
+from eduAPI import app
 
 client = TestClient(app)
 
-# Cual es el objetivo de testear?
-# Que se quiere testear?
-
 # Run the test: pytest
 
+### Get tests
+
+def test_get_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message" : "Hello World"}
+
+def test_get_item():
+    response = client.get("/item/0")
+    assert response.status_code == 200
+    assert response.json() == {"name" : "name", "description": "description", "price" : 1000}
+
+def test_get_not_inventory_item():
+    response = client.get("/item/1") # Puedo poner un -1?
+    assert response.status_code == 404
+    assert response.json() == {"detail" : "Item ID not found."}
+
+"""def test_get_by_name():
+    response = client.get("item/get-by-name?name=name")
+    assert response.status_code == 200
+    assert response.json() == {"name" : "name", "description": "description", "price" : 1000}
+
+def test_get_by_name_not_inventory():
+    response = client.get("item/get-by-name?name=test") # Importa si pongo name = test?
+    assert response.status_code == 404
+    assert response.json() == {"detail" : "Item name not found."}"""
+
+### Post tests
+
 def test_create_existing_item():
+    response = client.post("/create-item/0", json = {"name" : "name", "description": "description", "price" : 1000})
+    assert response.status_code == 400
+    assert response.json() == {"detail" : "Item ID already exists."}
+
+def test_create_item():
+    response = client.post("/create-item/2", json = {"name" : "test", "description": "description", "price" : 0})
+    assert response.status_code == 200
+    assert response.json() == {"name" : "test", "description": "description", "price" : 0}
+
+### Put tests
+
+def test_update_item():
+    response = client.put("/update-item/0", json = {"name" : "test", "description": "description", "price" : 0})
+    assert response.status_code == 200
+    assert response.json() == {"name" : "test", "description": "description", "price" : 0}
+
+def test_update_not_inventory_item():
+    response = client.put("/update-item/3", json = {"name" : "test", "description": "description", "price" : 0}) # Puedo poner -1?
+    assert response.status_code == 404
+    assert response.json() == {"detail" : "Item ID does not exists."}
+
+### Delete tests    
+
+def test_delete_item():
+    response = client.delete("/delete-item?item_id=0")
+    assert response.status_code == 200
+    assert response.json() == {"Success" : "Item deleted"}
+
+def test_delete_not_inventory_item():
+    response = client.delete("delete-item?item_id=4")
+    assert response.status_code == 404
+    assert response.json() == {"detail" : "Item ID does not exists."}
